206def484762997a1d396934ff07410ca2108683
francoishill <francoishill@e930bcc6-d1a7-0410-987c-80e4610ab126>
2011-10-05 10:51:10 +0000

    Disabled the FullRowSelect in the treeview of ViewBackups.cs.
    git-svn-id: svn://localhost/monitorsystem/trunk@40 e930bcc6-d1a7-0410-987c-80e4610ab126

--- MonitorSystem/MonitoredFilesChanged.Designer.cs
+++ MonitorSystem/MonitoredFilesChanged.Designer.cs
@@ -46,9 +46,9 @@
 			// 
 			// splitContainer1
 			// 
-			this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-									| System.Windows.Forms.AnchorStyles.Left)
-									| System.Windows.Forms.AnchorStyles.Right)));
+			this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.splitContainer1.Location = new System.Drawing.Point(12, 12);
 			this.splitContainer1.Name = "splitContainer1";
 			// 
