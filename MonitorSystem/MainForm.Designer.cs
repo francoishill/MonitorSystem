ef4174eff35e9eebf7f9e54ef4162692b545f79f
francoishill <francoishill@e930bcc6-d1a7-0410-987c-80e4610ab126>
2011-10-22 00:31:06 +0000

    Not much changed.
    git-svn-id: svn://localhost/monitorsystem/trunk@69 e930bcc6-d1a7-0410-987c-80e4610ab126

--- MonitorSystem/MainForm.Designer.cs
+++ MonitorSystem/MainForm.Designer.cs
@@ -420,7 +420,7 @@
 			this.scintilla_DummyScintilla.UseFont = true;
 			this.scintilla_DummyScintilla.Visible = false;
 			// 
-			// Form1
+			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -447,10 +447,9 @@
 			this.Controls.Add(this.textBoxLogs);
 			this.Controls.Add(this.linkLabelGetCurrentTodolist);
 			this.Controls.Add(this.linkLabel1);
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
 			this.KeyPreview = true;
 			this.MinimumSize = new System.Drawing.Size(691, 365);
-			this.Name = "Form1";
+			this.Name = "MainForm";
 			this.ShowInTaskbar = false;
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Monitor System";
